e6410447c44c4009eb92b47851219a131a26d239
Agnibha Mukherjee <b5x003agnibha.mukherjee@gmail.com>
2026-05-27 18:39:06 +0530

    chore: combine API and Worker into single free web service

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Start Celery worker in the background
+celery -A worker.celery_app worker --loglevel=info &
+
+# Start FastAPI API in the foreground
+uvicorn main:app --host 0.0.0.0 --port $PORT
